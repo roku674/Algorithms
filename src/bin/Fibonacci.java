787358a91b8e832c35f6e0fe4275aaa48ffabec0
roku674 <af03848@georgiasouthern.edu>
2019-02-20 18:15:07 -0500

    Fibbonacci
    1

---
+++ src/bin/Fibonacci.java
@@ -0,0 +1,23 @@
+package bin;
+import java.util.Scanner;
+
+//homework 1.5
+class Fibonacci
+{ 
+	static int fib(int n) 
+	{ 
+	if (n <= 1) 
+	return n; 
+	return fib(n-1) + fib(n-2); 
+	} 
+	
+	public static void main (String args[]) 
+	{ 
+		System.out.println("where f is Fibbonaci, f(x) = ? | type in a number for x");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		System.out.println("f(" + n + ") = " + fib(n)); 
+	}	
+} 
+
+
