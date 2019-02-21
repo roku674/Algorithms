d4782273292407a65aa2e2189d05a3e10fd9d6e6
roku674 <af03848@georgiasouthern.edu>
2019-02-20 19:23:02 -0500

    Update Fibonacci.java

--- src/bin/Fibonacci.java
+++ src/bin/Fibonacci.java
@@ -6,9 +6,9 @@ class Fibonacci
 { 
 	static int fib(int n) 
 	{ 
-	if (n <= 1) 
-	return n; 
-	return fib(n-1) + fib(n-2); 
+		if (n <= 1) 
+			return n; 
+		return fib(n-1) + fib(n-2); 
 	} 
 	
 	public static void main (String args[]) 
